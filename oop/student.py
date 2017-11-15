271aa89efaf6fa9a89645ac5fbbdf715e7141700
zhoupengzu <zhoupengzu@maizijf.com>
2017-11-15 10:32:11 +0800

    类的定义，只能有一个init，需要的属性可以在init中添加，self表示最近

---
+++ oop/student.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+"""
+只能有个一个构造函数
+"""
+class Student:
+    def __init__(self,name,score):
+        self.name = name
+        self.score = score
+    # def __init__(self):
+    #     self.name = "default"
+    #     self.score = 0
+
+stu = Student("zhoupengzu",99)
+print "姓名：%s,分数：%s" % (stu.name,stu.score)
