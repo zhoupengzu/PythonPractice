b59bd7319889a534819407fcca1f1ef3d1d2380c
zhoupengzu <zhoupengzu@maizijf.com>
2017-11-15 11:07:15 +0800

    继承、多态

---
+++ oop/Inherit.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+
+
+class Animal:
+    def __init__(self):
+        pass
+
+    def run(self):
+        print "Animal is running"
+
+
+class Dog(Animal):     #小括号里的表示继承的父类
+    def eat(self):     #添加自己的方法
+        print "dog is eatting"
+
+
+class Cat(Animal):
+    def run(self):    #重写父类的方法，调用的时候总是会优先调用该方法，即多态
+        print "cat is running"
+'''
+错误写法
+'''
+    # def run(self,name):
+    #     pass
+
+dog = Dog()
+dog.run()
+dog.eat()
+
+cat = Cat()
+cat.run()
+
+print isinstance(dog, Animal)
