f3672f680dfb75d1fd564f18f25d37646a9896ec
zhoupengzu <zhoupengzu@maizijf.com>
2017-11-16 14:55:57 +0800

    connect with php

--- Request/request_04.py
+++ Request/request_04.py
@@ -1,9 +1,19 @@
 # coding=utf-8
 
 import urllib2
+import json
 
-base_url = "http://localhost:8080/python_request/HelloPython.php"
+base_url = "http://localhost:8080/python_request/HelloPython.php?name=zhoupengzu"
 req = urllib2.Request(base_url)
 req_open = urllib2.urlopen(req)
 data = req_open.read()
-print data
+dic = data
+# if isinstance(data, unicode):
+#     print "is instance"
+#     dic = data
+# else:
+#     temp_dic = json.loads(data, encoding="utf-8")
+#     if isinstance(temp_dic, dict):
+#         print "dict"
+#     dic = temp_dic
+print dic
