4eee0ab2e0604ea1367e3ed26f6a443d2840328d
zhoupengzu <zhoupengzu@maizijf.com>
2018-01-15 10:03:29 +0800

    global和nonlocal的使用

--- Practice/practice28.py
+++ Practice/practice28.py
@@ -1,6 +1,11 @@
 
+global_var = 20
+
 def out_func():
     out = 10
+    # nonlocal global_var
+    global global_var
+    global_var = global_var + 2
     def in_func():
         # global out # 这里不能这么用，需要按照下面走
         nonlocal out
